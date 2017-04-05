569157d55750360dff5d805bfe0407a85d405081
JonCass <JCass1980@gmail.com>
2017-04-04 21:41:45 -0700

    all working

--- regex.js
+++ regex.js
@@ -187,14 +187,16 @@ console.assert(markDownItalics('random *asterisk') === 'random *asterisk');
 
 // Return an array of phone numbers from a given string
 function extractPhoneNumber (text) {
-
+    var regEx = /\(?(\d{3})\)?[\.\- ]?(\d{3})[\.\- ]?(\d{4})/g
+    return text.match(regEx)
 }
 
 console.assert(extractPhoneNumber("Dear Mr. Davis, I got to know of your company through our mutual friend Fiona Williams and the training you offer to graduate students in Advertising. I am a graduate student of Mass Communications with specialization in Advertising.  I am currently pursuing the last year of my course. I would very much like to see firsthand the work environment in an advertising agency. If you would like a reference, my advisor can be reached at (454) 999-1212. You can contact me at (919) 123-4569 at your convenience.").equals(["(454) 999-1212", "(919) 123-4569"]))
 
 // Return an array of all emails found inside of given string
 function extractEmails (text) {
-
+    var regEx = /[\.\w]+@\w+\.\w{2,}/g
+    return text.match(regEx)
 }
 
 console.assert(extractEmails("Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth@gmail.com tatsoi tomatillo azuki bean garlic. Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea@sprouts.org fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber.earthnut@pea.net peanut soko zucchini.").equals(["amaranth@gmail.com","pea@sprouts.org", "cucumber.earthnut@pea.net"]));
